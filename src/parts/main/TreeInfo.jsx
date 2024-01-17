70207d1ddb9f07f5a711a8a76c09e153e3149e16
ingbinsee <ingbinsee@gmail.com>
2024-01-17 16:04:02 +0900

    [Refactor] 메인 페이지 수정
    물방울 수에 따른 이미지 조건 처리를 switch/case문으로 변경하였습니다.

--- src/parts/main/TreeInfo.jsx
+++ src/parts/main/TreeInfo.jsx
@@ -7,33 +7,62 @@ import step6 from '@/assets/tree/step6.png';
 import step7 from '@/assets/tree/step7.png';
 import step8 from '@/assets/tree/step8.png';
 import step9 from '@/assets/tree/step9.png';
-import { number, string } from 'prop-types';
+import {number, string} from 'prop-types';
 
-function TreeInfo({waterNum, name}) {
+function TreeInfo({waterNum, name, src, water}) {
+  if (waterNum <= 1) {
+    water = '1단계';
+  } else if (waterNum <= 5) {
+    water = '2단계';
+  } else if (waterNum <= 10) {
+    water = '3단계';
+  } else if (waterNum <= 20) {
+    water = '4단계';
+  } else if (waterNum <= 30) {
+    water = '5단계';
+  } else if (waterNum <= 40) {
+    water = '6단계';
+  } else if (waterNum <= 60) {
+    water = '7단계';
+  } else if (waterNum <= 80) {
+    water = '8단계';
+  } else if (waterNum > 80) {
+    water = '9단계';
+  }
+
+  switch (water) {
+    case '2단계':
+      src = step2;
+      break;
+    case '3단계':
+      src = step3;
+      break;
+    case '4단계':
+      src = step4;
+      break;
+    case '5단계':
+      src = step5;
+      break;
+    case '6단계':
+      src = step6;
+      break;
+    case '7단계':
+      src = step7;
+      break;
+    case '8단계':
+      src = step8;
+      break;
+    case '9단계':
+      src = step9;
+      break;
+    default:
+      src = step1;
+      break;
+  }
+
   return (
     <figure>
-      <img
-        src={
-          waterNum <= 1
-            ? step1
-            : waterNum <= 5
-            ? step2
-            : waterNum <= 10
-            ? step3
-            : waterNum <= 20
-            ? step4
-            : waterNum <= 30
-            ? step5
-            : waterNum <= 40
-            ? step6
-            : waterNum <= 60
-            ? step7
-            : waterNum <= 80
-            ? step8
-            : step9
-        }
-        alt={`${name}의 나무`}
-      />
+      <img src={src} alt={`${name}의 나무`} />
       <figcaption>
         <h3>{name}</h3>
       </figcaption>
@@ -44,6 +73,8 @@ function TreeInfo({waterNum, name}) {
 TreeInfo.propTypes = {
   waterNum: number,
   name: string,
+  src: string,
+  water: string,
 };
 
 export default TreeInfo;
