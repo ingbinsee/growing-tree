b12b3984cbf2dcdcca33fc11b15ea560e6390a3c
ingbinsee <ingbinsee@gmail.com>
2024-01-17 19:11:52 +0900

    [Setting] 이미지 최적화 설정
    이미지 최적화를 위한 설정을 진행하였습니다.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 import {env} from 'node:process';
 import {resolve} from 'node:path';
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngQuant from 'imagemin-pngquant';
+import imageminSvgo from 'imagemin-svgo';
+import imageminWebp from 'imagemin-webp';
 
 const idDev = env.NODE_ENV === 'development';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+        png: imageminPngQuant(),
+        svg: imageminSvgo(),
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+          png: imageminWebp(),
+        },
+      },
+    }),
+  ],
   css: {
     devSourcemap: true,
     modules: {
@@ -20,4 +40,14 @@ export default defineConfig({
       '@': resolve(__dirname, './src'),
     },
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-helmet-async'],
+          reactRouter: ['react-router-dom'],
+        },
+      },
+    },
+  },
+});
