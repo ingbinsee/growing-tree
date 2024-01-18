e26ff802ae4a70df6fccec34fd3719679455a75b
ingbinsee <ingbinsee@gmail.com>
2024-01-18 21:46:29 +0900

    [Fix] 로고 이미지 렌더링되지 않는 에러 해결
    불특정 상황에서 로고 이미지가 렌더링되지 않는 문제를 해결하였습니다.

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,10 +1,15 @@
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import styles from './RootLayout.module.css';
+import logo from '@/assets/logo.png';
 
 function Header() {
   return (
     <header className={styles.header}>
-      <Link to="/main"><h1><img src="logo.png" alt="그로잉" /></h1></Link>
+      <Link to="/main">
+        <h1>
+          <img src={logo} alt="그로잉" />
+        </h1>
+      </Link>
     </header>
   );
 }
